f05ded7bffdbf7cac86742a4d8f088eb662d0159
Vikash Gupta <vikash9c35@gmail.com>
2025-01-12 11:33:53 +0530

    Create Product.js

---
+++ js/models/Product.js
@@ -0,0 +1,12 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  image: String,
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
